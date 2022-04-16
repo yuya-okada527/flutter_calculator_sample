092a7d18c3b6d04ce23928c72e9f5625f536aa2b
yuya-okada527 <yuya.okada527@gmail.com>
2022-04-16 23:37:37 +0900

    add timer picker

--- lib/pages/timer/timer_page.dart
+++ lib/pages/timer/timer_page.dart
@@ -1,12 +1,28 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class TimerPage extends StatelessWidget {
+class TimerPage extends StatefulWidget {
   const TimerPage({Key? key}) : super(key: key);
+
+  @override
+  _TimerPageState createState() => _TimerPageState();
+}
+
+class _TimerPageState extends State<TimerPage> {
+  Duration _duration = Duration.zero;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Timer"),
+      ),
+      body: Center(
+        child: CupertinoTimerPicker(
+          onTimerDurationChanged: (Duration duration) {
+            _duration = duration;
+          },
+          initialTimerDuration: _duration,
+        ),
       ),
     );
   }
