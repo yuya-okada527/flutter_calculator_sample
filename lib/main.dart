2f525fe6c6b7e8cc0d7fe8adcdafe33893ddf591
yuya-okada527 <yuya.okada527@gmail.com>
2022-04-17 17:08:40 +0900

    dotenv sample

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'pages/home_page.dart';
 
-void main() {
+void main() async {
+  await dotenv.load();
   runApp(const SevenToolsApp());
 }
 
--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    String val = dotenv.env['ENV_SAMPLE'] ?? "";
     return Scaffold(
       appBar: AppBar(
         title: const Text("Login"),
+      ),
+      body: Center(
+        child: Text("ENV_SAMPLE = " + val),
       ),
     );
   }
