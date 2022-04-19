e64e8860298a6d6eb2ada348b273bdb9f716a7c9
yuya-okada527 <yuya.okada527@gmail.com>
2022-04-20 00:31:22 +0900

    fix floating point bugs

--- lib/utils/expression_utils.dart
+++ lib/utils/expression_utils.dart
@@ -1,19 +1,83 @@
-import 'package:expressions/expressions.dart';
+import 'package:decimal/decimal.dart';
+import 'package:flutter/material.dart';
 
 class ExpressionUtils {
   static String eval(String expressionStr) {
-    var expression = Expression.parse(_convert(expressionStr));
-    var evaluator = const ExpressionEvaluator();
-    var answer = evaluator.eval(expression, {});
-    return answer.toString();
+    String expression = _convert(expressionStr);
+    expression = evalMultiplyOrDevide(expression);
+    return evalAdditionOrSubtraction(expression);
   }
 
   static String _convert(String expressionStr) {
     var result = expressionStr.replaceAll("×", "*");
     result = result.replaceAll("÷", "/");
-    result = result.replaceAll("% ", "* 0.01");
+    result = result.trim();
+    result = result.replaceAll("%", "* 0.01");
     // ignore: avoid_print
     print("expression: '$result'");
     return result;
+  }
+
+  @visibleForTesting
+  static String evalAdditionOrSubtraction(String expression) {
+    if (!_hasAddtionOrSubtraction(expression)) {
+      return expression;
+    }
+    List<String> tokens = expression.split(" ");
+    int index = tokens.indexWhere((token) => ["+", "-"].contains(token));
+    if (index == -1) {
+      return expression;
+    }
+    if (index == 0 || index == tokens.length - 1) {
+      throw Exception("math expression is invalid");
+    }
+    var num = _calc(tokens[index - 1], tokens[index + 1], tokens[index]);
+    List<String> nextTokens = tokens.sublist(0, index - 1) +
+        [num.toString()] +
+        tokens.sublist(index + 2);
+    return evalAdditionOrSubtraction(nextTokens.join(" "));
+  }
+
+  @visibleForTesting
+  static String evalMultiplyOrDevide(String expression) {
+    if (!_hasMultiplierOrDevider(expression)) {
+      return expression;
+    }
+    List<String> tokens = expression.split(" ");
+    int index = tokens.indexWhere((token) => ["*", "/"].contains(token));
+    if (index == -1) {
+      return expression;
+    }
+    if (index == 0 || index == tokens.length - 1) {
+      throw Exception("math expression is invalid");
+    }
+    var num = _calc(tokens[index - 1], tokens[index + 1], tokens[index]);
+    List<String> nextTokens = tokens.sublist(0, index - 1) +
+        [num.toString()] +
+        tokens.sublist(index + 2);
+    return evalMultiplyOrDevide(nextTokens.join(" "));
+  }
+
+  static dynamic _calc(right, left, operator) {
+    switch (operator) {
+      case "+":
+        return Decimal.parse(right) + Decimal.parse(left);
+      case "-":
+        return Decimal.parse(right) - Decimal.parse(left);
+      case "*":
+        return Decimal.parse(right) * Decimal.parse(left);
+      case "/":
+        return Decimal.parse(right) / Decimal.parse(left);
+      default:
+        throw Exception("$operator is not supported");
+    }
+  }
+
+  static bool _hasMultiplierOrDevider(String expression) {
+    return expression.contains("*") || expression.contains("/");
+  }
+
+  static bool _hasAddtionOrSubtraction(String expression) {
+    return expression.contains("+") || expression.contains("-");
   }
 }
--- test/utils/expression_utils_test.dart
+++ test/utils/expression_utils_test.dart
@@ -2,57 +2,77 @@ import 'package:flutter_calculator/utils/expression_utils.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group("単純四則演算", () {
-    test('1 + 1 = 2', () {
-      expect(ExpressionUtils.eval("1 + 1"), "2");
+  group("eval", () {
+    group("単純四則演算", () {
+      test('1 + 1 = 2', () {
+        expect(ExpressionUtils.eval("1 + 1"), "2");
+      });
+
+      test('3 - 2 = 1', () {
+        expect(ExpressionUtils.eval("3 - 2"), "1");
+      });
+
+      test('3 * 2 = 6', () {
+        expect(ExpressionUtils.eval("3 * 2"), "6");
+      });
+
+      test('4 / 2 = 2.0', () {
+        expect(ExpressionUtils.eval("4 / 2"), "2");
+      });
+
+      test('10 % = 0.1', () {
+        expect(ExpressionUtils.eval("10 % "), "0.1");
+      });
+
+      test('10 % = 0.1', () {
+        expect(ExpressionUtils.eval("10 %"), "0.1");
+      });
     });
 
-    test('3 - 2 = 1', () {
-      expect(ExpressionUtils.eval("3 - 2"), "1");
+    group("複雑四則演算", () {
+      test("1 + 2 + 3 = 6(加減のみ)", () {
+        expect(ExpressionUtils.eval("1 + 2 + 3"), "6");
+      });
+
+      test("1 * 2 + 3 = 5(乗除あり 順番通り)", () {
+        expect(ExpressionUtils.eval("1 * 2 + 3"), "5");
+      });
+
+      test("1 + 4 / 2 = 3.0(乗除あり 順番通り)", () {
+        expect(ExpressionUtils.eval("1 + 4 / 2"), "3");
+      });
     });
 
-    test('3 * 2 = 6', () {
-      expect(ExpressionUtils.eval("3 * 2"), "6");
-    });
+    group("端数あり", () {
+      test("1.1 + 2.3 = 3.4", () {
+        expect(ExpressionUtils.eval("1.1 + 2.3"), "3.4");
+      });
 
-    test('4 / 2 = 2.0', () {
-      expect(ExpressionUtils.eval("4 / 2"), "2.0");
+      test("0.2 * 3 = 0.6", () {
+        expect(ExpressionUtils.eval("0.2 * 3"), "0.6");
+      });
     });
-
-    test('10 % = 0.1', () {
-      expect(ExpressionUtils.eval("10 % "), "0.1");
-    });
-
-    // ignore: todo
-    // TODO: スペースがないと失敗する
-    // test('10 % = 0.1', () {
-    //   expect(ExpressionUtils.eval("10 %"), "0.1");
-    // });
   });
 
-  group("複雑四則演算", () {
-    test("1 + 2 + 3 = 6(加減のみ)", () {
-      expect(ExpressionUtils.eval("1 + 2 + 3"), "6");
+  group("_evalMultiplierOrDevider", () {
+    test("1 + 2", () {
+      expect(ExpressionUtils.evalMultiplyOrDevide("1 + 2"), "1 + 2");
     });
 
-    test("1 * 2 + 3 = 5(乗除あり 順番通り)", () {
-      expect(ExpressionUtils.eval("1 * 2 + 3"), "5");
+    test("1 * 2", () {
+      expect(ExpressionUtils.evalMultiplyOrDevide("1 * 2"), "2");
     });
 
-    test("1 + 4 / 2 = 3.0(乗除あり 順番通り)", () {
-      expect(ExpressionUtils.eval("1 + 4 / 2"), "3.0");
-    });
-  });
-
-  group("端数あり", () {
-    test("1.1 + 2.3 = 3.4", () {
-      expect(ExpressionUtils.eval("1.1 + 2.3"), "3.4");
+    test("1 * 2 * 3", () {
+      expect(ExpressionUtils.evalMultiplyOrDevide("1 * 2 * 3"), "6");
     });
 
-    // ignore: todo
-    // TODO: 端数が出る場合はNG
-    // test("0.2 * 3 = 0.6", () {
-    //   expect(ExpressionUtils.eval("0.2 * 3"), "0.6");
-    // });
+    test("1 + 2 * 3", () {
+      expect(ExpressionUtils.evalMultiplyOrDevide("1 + 2 * 3"), "1 + 6");
+    });
+
+    test("1 + 4 / 2", () {
+      expect(ExpressionUtils.evalMultiplyOrDevide("1 + 4 / 2"), "1 + 2");
+    });
   });
 }
