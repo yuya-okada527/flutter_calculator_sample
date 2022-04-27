63e02714c162431239ca81d0d0aa524409f9c620
yuya-okada527 <yuya.okada527@gmail.com>
2022-04-27 23:42:14 +0900

    add tests

--- test/pages/calculator/calculator_page_test.dart
+++ test/pages/calculator/calculator_page_test.dart
@@ -4,21 +4,38 @@ import 'package:flutter_calculator/widgets/calculator/calculator_button.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets("CalculatorPage", (WidgetTester tester) async {
-    await tester.pumpWidget(const MaterialApp(
-      home: Calculator(),
-    ));
+  for (var condition in [
+    {
+      "commands": ["1", "0", "+", "2", "="],
+      "expected": ["12"]
+    },
+    {
+      "commands": ["1", "0", "+", "2", "-", "1", "="],
+      "expected": ["11"]
+    },
+    {
+      "commands": ["1", "0", "×", "2", "="],
+      "expected": ["20"]
+    },
+    {
+      "commands": ["3", "0", "÷", "2", "="],
+      "expected": ["15"]
+    }
+  ]) {
+    testWidgets("CalculatorPage", (WidgetTester tester) async {
+      await tester.pumpWidget(const MaterialApp(
+        home: Calculator(),
+      ));
 
-    // アイコンが10+2を計算
-    await tester.tap(find.widgetWithText(CalculatorButton, "1"));
-    await tester.tap(find.widgetWithText(CalculatorButton, "0"));
-    await tester.tap(find.widgetWithText(CalculatorButton, "+"));
-    await tester.tap(find.widgetWithText(CalculatorButton, "2"));
-    await tester.tap(find.widgetWithText(CalculatorButton, "="));
+      List<String> commands = condition["commands"] ?? [];
+      for (var command in commands) {
+        await tester.tap(find.widgetWithText(CalculatorButton, command));
+      }
 
-    // Widgetツリーの再構築
-    await tester.pump();
+      // Widgetツリーの再構築
+      await tester.pump();
 
-    expect(find.text('12'), findsOneWidget);
-  });
+      expect(find.text(condition["expected"]!.first), findsOneWidget);
+    });
+  }
 }
